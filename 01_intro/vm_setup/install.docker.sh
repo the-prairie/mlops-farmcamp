8aefc4b4e2f4439654e989a8947f6eccaacf2e84
Lauren Zary <lzary@atb.com>
2022-05-18 00:11:06 -0700

    shell script to install docker onto VM

---
+++ 01_intro/vm_setup/install.docker.sh
@@ -0,0 +1,23 @@
+
+   # Update the apt package index and install packages to allow apt to use a repository over HTTPS
+sudo apt update
+sudo apt upgrade
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+
+# Add Docker’s official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - 
+
+# The following command is to set up the stable repository
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu `lsb_release -cs` stable" 
+
+# Update the apt package index, and install the latest version of Docker Engine and containerd, or go to the next step to install a specific version
+sudo apt update 
+sudo apt install -y docker-ce
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+
+# Ubuntu comes with Python3 installed but need pip for package management
+sudo apt update 
+sudo apt install -y python3-dev python3-venv python3-pip
