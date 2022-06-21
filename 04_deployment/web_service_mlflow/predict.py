0521150a644c48fa27e77f160475a81a0ab3e055
Lauren Zary <lzary@atb.com>
2022-06-21 01:42:34 -0700

    Deploy with fastapi

--- 04_deployment/web_service_mlflow/predict.py
+++ 04_deployment/web_service_mlflow/predict.py
@@ -2,6 +2,19 @@ import pickle
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+import mlflow
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+MLFLOW_TRACKING_URI = os.environ.get("MLFLOW_TRACKING_URI")
+mlflow.set_tracking_uri(f"{MLFLOW_TRACKING_URI}:4040")
+
+model_name = "SklearnRFGreenTaxi"
+model_version = 2
+model = mlflow.pyfunc.load_model(
+    model_uri=f"models:/{model_name}/{model_version}"
+)
 
 
 app = FastAPI()
@@ -13,23 +26,20 @@ class Ride(BaseModel):
     
 class PredictedRideDuration(BaseModel):
     PREDICTED_DURATION: float
-
-with open ("models/linreg_model.bin", "rb") as f_in:
-   (dv, model) = pickle.load(f_in)
    
 
 def prepare_features(ride):
     features = {}
-    features["ROUTE"] = f'{ride["PULOCATIONID"]}_{ride["DOLOCATIONID"]}'
+    features["ROUTE"] = f'{ride["PULOCATIONID"].astype(str)}_{ride["DOLOCATIONID"].astype(str)}'
     features["TRIP_DISTANCE"] = ride["TRIP_DISTANCE"]
     return features
     
     
 def predict(features):
-    X = dv.transform(features)
-    predictions = model.predict(X)
+    predictions = model.predict(features)
     
     return predictions[0]
+
 
 @app.get("/")
 def read_root():
@@ -43,6 +53,8 @@ async def predict_ride(ride: Ride):
     prediction = predict(features=features)
     
     
-    return {"Ride duration":prediction}
-
-
+    return {
+        "Ride duration": prediction,
+        "Model": model_name,
+        "Model Version": model_version
+    }
